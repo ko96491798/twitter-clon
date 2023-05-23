fe5a177ea4016cbb1e36d041543644afacf79d7b
ko96491798 <ddsl701@gmail.com>
2023-05-24 00:18:05 +0800

    first

---
+++ components/Layout.tsx
@@ -0,0 +1,30 @@
+// 側邊欄、主要內容區域和關注欄
+import React from "react";
+
+import FollowBar from "@/components/layout/FollowBar";
+import Sidebar from "@/components/layout/Sidebar";
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <Sidebar />
+          <div
+            className="
+              col-span-3 
+              lg:col-span-2 
+              border-x-[1px] 
+              border-neutral-800
+          "
+          >
+            {children}
+          </div>
+          <FollowBar />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
